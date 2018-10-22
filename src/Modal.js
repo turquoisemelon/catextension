ac7652e3bf521d88b660f30135071bcb39ce3d8b
Meltem <melltemkilic@gmail.com>
2018-10-21 22:44:28 -0400

    WIP-Refactor modal closing and img rendering logic

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Modal.css';
 
-export const Modal = () => (
-    <div class="_extension-modal">
-        <div class="_extension-modal-content"></div>
-    </div>);
+export class Modal extends Component {
+    closeModal(e) {
+        if (e.target == this.modal || e.target == this.content) {
+            this.modal.style.display = "none";
+        }
+        return;
+    }
+
+    render() {
+        return (
+            <div class="_extension-modal"
+                ref={(node) => {this.modal=node}}
+                onClick={(ev) => this.closeModal(ev)}           
+            >
+                <div class="_extension-modal-content">
+                    {this.props.imgUrl ? <img src={`${this.props.imgUrl}`} /> : null}
+                </div>
+            </div>)
+    }
+};
--- src/script.js
+++ src/script.js
@@ -2,26 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Modal } from './Modal.js';
 
-const renderModalToDOM = (modal) => {
-    const bodyEl = document.getElementsByTagName('body')[0];
-    const modalWrapper = document.createElement("div");
-    bodyEl.appendChild(modalWrapper);
+const bodyEl = document.getElementsByTagName('body')[0];
+const modalWrapper = document.createElement("div");
+modalWrapper.className = "_extension_modal_wrapper"
+bodyEl.appendChild(modalWrapper);
 
-    ReactDOM.render(modal, modalWrapper);
+const renderModalToDOM = (modal) => {
+    const wrapper= document.getElementsByClassName("_extension_modal_wrapper")[0];
+    ReactDOM.render(modal, wrapper);
+    document.getElementsByClassName("_extension-modal")[0].style.display = "flex";
     return;
 };
-
-renderModalToDOM(<Modal/>);
-
-const insertGifToDOM = (gifUrl) => {
-    if (gifUrl) {
-        const modalContentEl = document.getElementsByClassName('_extension-modal-content')[0];
-        const imageEl = `<img src="${gifUrl}"/>`;
-        modalContentEl.innerHTML = imageEl;
-        document.getElementsByClassName('_extension-modal')[0].style.display = "flex";
-    }
-    return;
-}
 
 const processRequest = (data) => {
     if (data.action === 'getApiData') {
@@ -52,7 +43,7 @@ chrome
                 ? gifphyData.data.image_url
                 : null;
 
-            insertGifToDOM(gifphyDataImageURL);
+            renderModalToDOM(<Modal imgUrl={gifphyDataImageURL}/>);
 
             sendResponse({response: value});
         }, (error) => {
@@ -63,11 +54,3 @@ chrome
 
         return true; // signal that it will respond asyncronously
     });
-
-// When the user clicks anywhere outside of the modal, close it
-const extensionModal = document.getElementsByClassName('_extension-modal')[0];
-window.onclick = function (event) {
-    if (event.target == extensionModal) {
-        extensionModal.style.display = "none";
-    }
-};
